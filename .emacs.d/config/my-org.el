dc8352431d6704b2b7394af5078bc17d36d73c63
Xavier del Castillo <xavier.delcastillo@sideinc.com>
2022-09-06 15:32:31 -0700

    org-roam config

--- .emacs.d/config/my-org.el
+++ .emacs.d/config/my-org.el
@@ -20,10 +20,19 @@
 	("j" "Journal" entry (file+datetree my-journal-org-file "Journal")
 	 "* %?\n %i\n %a")))
 
+(setq org-todo-keyword-faces
+      '(("TODO" . org-warning) ("INPROGRESS" . "yellow")
+        ("PR" . "yellow") ("STAGING" . "orange") ("DONE" . org-done)
+        ("DROPPED" . "blue")))
+
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "INPROGRESS(i)" "PR(p)" "STAGING(s)" "|" "DONE(d)")))
+
 (use-package org-roam
   :ensure t
   :custom
-  (org-roam-directory my-org-directory)
+  (org-roam-directory (concat my-org-directory "/roam"))
+  (org-roam-complete-everywhere t)
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
          ("C-c n g" . org-roam-graph)
@@ -31,7 +40,9 @@
          ("C-c n c" . org-roam-capture)
          ("C-c n j" . org-roam-dailies-capture-today))
   :config
-  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag))))
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (require 'org-roam-dailies)
+  (org-roam-db-autosync-mode))
 
 
 (provide 'my-org)
